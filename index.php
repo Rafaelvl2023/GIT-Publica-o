0dc38e3354015a5b6a173997c6665695d7add753
Rafael Teixeira <rafaelteixeiravl@gmail.com>
2024-07-08 09:17:38 -0300

    Primeira Publicação de Git

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GIT Publicação</title>
+</head>
+<body>
+    <h1>GIT publicação</h1>
+</body>
+</html>
+
